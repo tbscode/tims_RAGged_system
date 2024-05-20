404e4aa211418a92c7ffab209539f306204ea031
Tim Schupp <tim@timschupp.de>
2024-05-20 15:05:55 +0200

    added web-search node

--- system/rag/nodes/__init__.py
+++ system/rag/nodes/__init__.py
@@ -1,12 +1,13 @@
 from .extractor import ParamExtractorNode
 from .casual import CasualResponseNode
 from .tools import ToolSelectorNode, ToolCasualEndNode
-from .web import WebSearchLookup
+from .web import WebSearchLookup, WebSearchResponse
 
 __all__ = [
     "ParamExtractorNode",
     "CasualResponseNode",
     "ToolSelectorNode",
     "ToolCasualEndNode",
-    "WebSearchLookup"
+    "WebSearchLookup",
+    "WebSearchResponse"
 ]
--- system/rag/nodes/web.py
+++ system/rag/nodes/web.py
@@ -1,4 +1,7 @@
-from rag.abs import RagNode, NodeContext, RagNodeResult
+from rag.abs import RagNode, NodeContext, RagNodeResult, DEFAULT_MODEL
+from rag.models import get_model, get_client_for_model
+from serpapi import GoogleSearch
+import os
 
 class WebSearchLookup(RagNode):
 
@@ -13,10 +16,83 @@ class WebSearchLookup(RagNode):
         
         print(f"*** Web search query: {search_query}")
         
+        
+        params = {
+          "engine": "google",
+          "q": search_query,
+          "api_key": os.getenv("SERPAPI_API_KEY")
+        }
+
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        
         return RagNodeResult(
             node_name=self.name,
             forward=True,
             response={
-                "search_query": search_query
+                "search_query": search_query,
+                "search_results": results
             }
+        )
+        
+
+class WebSearchResponse(RagNode):
+    base_prompt = """
+Based on the users query, you used the 'web_search' tool to search the web for the query: {search_query}.
+
+Here are the search results:
+{web_search_results}
+
+Use them to answer the users query.
+Respond with a consize answer to the users query.
+"""
+
+    system_prompt = ""
+    
+    model_name = DEFAULT_MODEL
+    
+    def create(self, **kwargs):
+        self.model_name = kwargs.get("model", self.model_name)
+        self.model = get_model(self.model_name)
+
+
+    def run(self, context: NodeContext):
+        
+        print(f"*** Running {self.name}, context: {context.parent_results}")
+        
+        search_results = context.parent_results["WebSearchLookup"].response
+        
+        self.system_prompt = self.base_prompt.format(
+            search_query=search_results["search_query"],
+            web_search_results=search_results["search_results"]
+        )
+
+        messages = [{
+            "role": "system",
+            "content": self.base_prompt
+        }, {
+            "role": "user",
+            "content": context.prompt
+        }]
+        
+        print("Running ParamExtractorNode", messages)
+        
+        completion_params = {
+            "model": self.model.model,
+            "messages": messages,
+            "max_tokens": 400,
+            "temperature": 0.0,
+        }
+
+        client = get_client_for_model(self.model.model)
+        response = client.chat.completions.create(
+            **completion_params
+        )
+
+        res = response.choices[0].message.content
+        
+        return RagNodeResult(
+            node_name=self.name,
+            forward=True,
+            response=res        
         )
